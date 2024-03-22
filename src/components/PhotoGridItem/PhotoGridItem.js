42f1a46cc9cc4753382cc6da1e35eb7dee170a92
Aleksandra Mętrak <aleksandra.metrak@closbrothers.pl>
2024-03-22 10:09:21 +0100

    last task - unfinished

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -49,13 +49,20 @@ const Image = styled.img`
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
+  /* display: flex; */
+  /* flex-wrap: wrap; */
   gap: 8px;
+  white-space: nowrap;
+  overflow: hidden;
 `;
 
 const Tag = styled.li`
+  display: inline-block;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
   padding: 4px 8px;
+  margin-right: 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
